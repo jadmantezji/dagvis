fc899918599791c6254dda5f066612d2427a3a9c
niras <niras@localhost>
2009-10-06 04:46:30 +0200

    View factory now supports box shape.

--- src/main/java/pl/cyfronet/virolab/dagvis/view/jgraph/ViewFactory.java
+++ src/main/java/pl/cyfronet/virolab/dagvis/view/jgraph/ViewFactory.java
@@ -2,10 +2,15 @@ package pl.cyfronet.virolab.dagvis.view.jgraph;
 
 import org.jgraph.graph.CellView;
 import org.jgraph.graph.CellViewFactory;
+import org.jgraph.graph.DefaultGraphCell;
 import org.jgraph.graph.EdgeView;
+import org.jgraph.graph.GraphCell;
 import org.jgraph.graph.GraphModel;
 import org.jgraph.graph.PortView;
+import org.jgraph.graph.VertexView;
 
+import pl.cyfronet.virolab.dagvis.structure.INode;
+import pl.cyfronet.virolab.dagvis.util.Shape;
 import pl.cyfronet.virolab.dagvis.view.jgraph.view.EllipseVertexView;
 
 public class ViewFactory implements CellViewFactory {
@@ -13,12 +18,17 @@ public class ViewFactory implements CellViewFactory {
 	@Override
 	public CellView createView(GraphModel model, Object cell) {
 		CellView view = null;
+		GraphCell gc = (GraphCell) cell;
 		if (model.isPort(cell)) {
 			view = new PortView(cell);
 		} else if (model.isEdge(cell)) {
 			view = new EdgeView(cell);
 		} else {
-			view = new EllipseVertexView(cell);
+			Shape shape = ((INode) ((DefaultGraphCell) cell).getUserObject()).getShape();
+			switch (shape) {
+				case BOX : view = new VertexView(cell); break;
+				case ELLIPSE : view = new EllipseVertexView(cell); break;
+			}
 		}
 		return view;
 	}
