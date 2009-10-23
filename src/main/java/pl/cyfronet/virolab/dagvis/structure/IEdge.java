f5325eb5277c5e9b2054c29b611f2917d459e020
niras <niras@localhost>
2009-10-23 10:56:51 +0200

    (no commit message)

--- src/main/java/pl/cyfronet/virolab/dagvis/structure/IEdge.java
+++ src/main/java/pl/cyfronet/virolab/dagvis/structure/IEdge.java
@@ -6,14 +6,55 @@ import pl.cyfronet.virolab.dagvis.util.ArrowStyle;
 import pl.cyfronet.virolab.dagvis.util.LinePattern;
 
 /**
+ * A graph edge interface.
  * 
  * @author Krzysztof Nirski
  *
  */
 public interface IEdge {
 	
+	/**
+	 * Gets the name of the edge which is unique.
+	 * 
+	 * @return Name.
+	 */
 	String getName();
+	
+	/**
+	 * Sets the name. If an edge of that name exists in graph, the behavior is undefined.
+	 * 
+	 * @param name
+	 */
 	void setName(String name);
+	
+	/**
+	 * Gets the source node.
+	 * 
+	 * @return Source node.
+	 */
+	INode getSource();
+	
+	/**
+	 * Sets the source node.
+	 *  
+	 * @param source Source node.
+	 */
+	void setSource(INode source);
+	
+	/**
+	 * Gets the target node.
+	 * 
+	 * @return Target node.
+	 */
+	INode getTarget();
+	
+	/**
+	 * Sets the target node.
+	 *  
+	 * @param source Target node.
+	 */
+	void setTarget(INode target);
+	
 	String getLabel();
 	void setLabel(String label);
 	Color getColor();
@@ -24,9 +65,5 @@ public interface IEdge {
 	void setLinePattern(LinePattern pattern);
 	boolean isBold();
 	void setBold(boolean bold);
-	INode getSource();
-	void setSource(INode source);
-	INode getTarget();
-	void setTarget(INode target);
 	
 }
--- src/main/java/pl/cyfronet/virolab/dagvis/structure/INode.java
+++ src/main/java/pl/cyfronet/virolab/dagvis/structure/INode.java
@@ -5,14 +5,41 @@ import java.awt.Color;
 import pl.cyfronet.virolab.dagvis.util.Shape;
 
 /**
+ * A graph node interface.
  * 
  * @author Krzysztof Nirski
  *
  */
 public interface INode {
 	
+	/**
+	 * Gets the name of the node which is unique.
+	 * 
+	 * @return Name.
+	 */
 	String getName();
+	
+	/**
+	 * Sets the name. If a node of that name exists in graph, the behavior is undefined.
+	 * 
+	 * @param name
+	 */
 	void setName(String name);
+
+	/**
+	 * Checks the status of node.
+	 * 
+	 * @return True if node is active. Otherwise false.
+	 */
+	boolean isActive();
+	
+	/**
+	 * Sets the status of node.
+	 * 
+	 * @param h New status.
+	 */
+	void setActive(boolean h);
+	
 	String getLabel();
 	void setLabel(String label);
 	Color getColor();
@@ -23,7 +50,5 @@ public interface INode {
 	void setContourCount(int count);
 	boolean isBold();
 	void setBold(boolean bold);
-	boolean isActive();
-	void setActive(boolean h);
 
 }
