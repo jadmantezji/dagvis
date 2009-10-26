82335ed2a788f9746cdee091c343cefc81663c22
niras <niras@localhost>
2009-10-26 01:33:46 +0100

    (no commit message)

--- src/main/java/pl/cyfronet/virolab/dagvis/input/Transformer.java
+++ src/main/java/pl/cyfronet/virolab/dagvis/input/Transformer.java
@@ -5,13 +5,21 @@ import java.io.InputStream;
 import pl.cyfronet.virolab.dagvis.structure.IGraph;
 
 /**
- * More than meets the eye.
+ * Transformers: more than meets the eye.
+ * Interface for input data to abstract data structure transformer.
  * 
  * @author Krzysztof Nirski
  *
  */
 public interface Transformer {
-
+	
+	/**
+	 * Transforms source to IGraph instance.
+	 * 
+	 * @param source Stream of input data.
+	 * @return Abstract Graph representation.
+	 * @throws TransformationException When the input contains error. 
+	 */
 	IGraph getGraph(InputStream source) throws TransformationException;
 	
 }
